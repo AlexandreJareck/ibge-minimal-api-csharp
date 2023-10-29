932ddec3cd9df26a60b9e4e6a293c86f1492c311
AlexandreJareck <alexandrejareck@gmail.com>
2023-10-29 10:19:04 -0300

    feat: add location repo (not implemented)

---
+++ IBGE/Repository/LocationRepository.cs
@@ -0,0 +1,43 @@
+﻿using IBGE.Context;
+using IBGE.Entities;
+using IBGE.Interfaces;
+
+namespace IBGE.Repository
+{
+    public class LocationRepository : ILocationRepository
+    {
+        private readonly IbgeDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public LocationRepository(IbgeDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        public Task Add(Location location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InsertBatch(IEnumerable<Location> locations)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Location>> Search(string search)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Location location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
