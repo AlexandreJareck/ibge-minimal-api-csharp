e22f918ea66e40950b0688104c0d5de4211a07e8
AlexandreJareck <alexandrejareck@gmail.com>
2023-10-29 10:18:45 -0300

    feat: add interface location repository

---
+++ IBGE/Interfaces/ILocationRepository.cs
@@ -0,0 +1,17 @@
+﻿using IBGE.Entities;
+
+namespace IBGE.Interfaces
+{
+    public interface ILocationRepository
+    {
+        Task Add(Location location);
+
+        Task Update(Location location);
+
+        Task Delete(string id);
+
+        Task<int> InsertBatch(IEnumerable<Location> locations);
+
+        Task<IList<Location>> Search(string search);
+    }
+}
