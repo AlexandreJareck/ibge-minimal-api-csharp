da06b2fe7ae83aca1feed76d9369a7f64aeb13b8
AlexandreJareck <alexandrejareck@gmail.com>
2023-10-29 10:19:32 -0300

    refactor: alter name dto to viewmodel

---
+++ IBGE/ViewModel/LocationViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace IBGE.DTO
+{
+    public record LocationViewModel
+    {
+        public string Id { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+    }
+}
