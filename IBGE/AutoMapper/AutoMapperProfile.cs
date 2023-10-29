bf032a6b7e2ba9742f91578af5c734db30941657
AlexandreJareck <alexandrejareck@gmail.com>
2023-10-29 10:18:24 -0300

    feat: add automapper

---
+++ IBGE/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using IBGE.DTO;
+using IBGE.Entities;
+
+namespace Challenge.Balta.IBGE.MapperProfile
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<LocationViewModel, Location>().ReverseMap();
+        }
+    }
+}
