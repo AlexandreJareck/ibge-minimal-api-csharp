4b00d055640072b303f15aa373e9d9ee5fb58779
AlexandreJareck <alexandrejareck@gmail.com>
2023-10-30 07:39:04 -0300

    feat: add location service

---
+++ IBGE/Interfaces/ILocationService.cs
@@ -0,0 +1,17 @@
+﻿using IBGE.Entities;
+
+namespace IBGE.Interfaces
+{
+    public interface ILocationService
+    {
+        Task Add(Location location);
+
+        Task Update(Location location);
+
+        Task Delete(string id);
+
+        Task<IList<Location>> Search(string search);
+
+        Task<int> ProcessExcelFileAsync(IFormFile excelFile);
+    }
+}
---
+++ IBGE/Services/LocationService.cs
@@ -0,0 +1,33 @@
+﻿using IBGE.Entities;
+using IBGE.Interfaces;
+
+namespace IBGE.Services
+{
+    public class LocationService : ILocationService
+    {
+        public Task Add(Location location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> ProcessExcelFileAsync(IFormFile excelFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Location>> Search(string search)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Location location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
